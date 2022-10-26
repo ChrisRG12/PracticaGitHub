f48f464c0d47627a439c829b5b15d53c4b127660
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-26 15:09:58 -0500

    Tarjeta
    Metí un container afuera de la tarjeta, esto para que tenga mejor aparecía, y y se pueda mover y centrar la tarjeta.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,32 +4,31 @@
 @section('contenido')
 
 
- <h1 class="aling-text center" style="background-color: aliceblue"> Inicio actividad GitHub </h1>
+<div class="container col-md-10 offset-md-4">
+<br>
 
-
- <div class="card text-center mb-5 ml-5 fw-bold" style="width: 24rem; margin-left: 1rem">
+ <div class="card text-center mb-7  ml-5 fw-bold" style="width: 24rem; margin-left: 1rem">
                 
         
     <div class="card-header text-center">
-      
+        Inicio actividad GitHub
     </div>
-    
+   
 
     <div class="card-body">
-     
+       
+        <img src="\imagenes\in.jpg" alt="...">
+
     </div>
 
 
 
     <div class="card-footer">
-        
+       <a href="inicio"><button class="btn" style="background-color: rgb(135, 210, 245)"> Formulario </button></a>
     </div>
 
 
-</div> 
-
-     
-    
+</div>    
 
 </div>
 
