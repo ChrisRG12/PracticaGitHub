643b56494c4990b465ace6d2e7a26daf6dc265bd
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-26 14:31:37 -0500

    Cambiamos el color de fondo
    En la plantilla cambiamos el fondo para que tenga una mejor aparecía dentro de los fondos y las vistas.

---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> GitHub </title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    
+</head>
+
+
+
+<body>
+    <style>
+body{
+    background: #373B44;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, rgb(7, 51, 121), #373B44);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #0d387e, #373B44); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+}
+    </style> 
+
+
+      @yield('contenido')
+
+
+
+    
+    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('plantilla')
+
+
+@section('contenido')
+
+
+ <h1> Inicio actividad GitHub </h1>
+
+
+   
+  
+
+     
+    
+
+</div>
+
+@stop
