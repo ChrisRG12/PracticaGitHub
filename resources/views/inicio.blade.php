a8fb70144791da286fabee2e40dcafd35fd91383
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-26 16:03:52 -0500

    Tabla
    En la segunda tarjeta agregamos una tabla de Bootstrap y modificando el borde y la posición de la tarjeta

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,7 +3,7 @@
 
 @section('contenido')
 
-<h1>Inicio</h1>
+<h1 class="aling-text center">Inicio</h1>
 
 <div class="row">
     <div class="col-4 ">
@@ -31,20 +31,46 @@
     <div class="col-4">
         <div class="container col-md-12" > 
 
-            <div class="card text-center mb-5 fw-bold" style="width: 24rem; margin-left: 20rem">
+            <div class="card text-center mb-5 fw-bold" style="width: 34rem; margin-left: 20rem">
                 
                 <div class="card-header text-center">
                 Tabla
                 </div>
                 
                 <div class="card-body">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                          </tr>
+                        </tbody>
+                      </table>
 
                 </div>
         
         
-        
-                <div class="card-footer">
-                </div>
         
         
             </div> 
