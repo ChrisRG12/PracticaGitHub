74b26996e49a8bf296567eac0f3f5f80b0ee0342
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-26 15:17:55 -0500

    Barra de Navegacion
    Agregamos una barra de navegación para que sea mas fácil moverse en las vistas dentro del proyecto

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -3,15 +3,55 @@
 
 @section('contenido')
 
+<h1>Inicio</h1>
 
+<div class="row">
+    <div class="col-4 ">
+        <div class="card text-center mb-5 ml-5 fw-bold" style="width: 24rem; margin-left: 1rem">
+            
+    
+            <div class="card-header text-center">
+                
+            </div>
+            
+    
+            <div class="card-body">
 
+            </div>
+    
+    
+    
+            <div class="card-footer">
+               
+            </div>
+    
+    
+        </div> 
+    </div>
+    <div class="col-4">
+        <div class="container col-md-12" > 
 
+            <div class="card text-center mb-5 fw-bold" style="width: 24rem; margin-left: 2rem">
+                
+                <div class="card-header text-center">
+                
+                </div>
+                
+                <div class="card-body">
 
-
-
-
-
-
+                </div>
+        
+        
+        
+                <div class="card-footer">
+                </div>
+        
+        
+            </div> 
+            
+        
+        </div>
+    </div>
 
 
 
