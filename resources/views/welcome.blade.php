7413577c0aaad59177fc5903358a3bf4d6d1952b
ChrisRG12 <116390968+ChrisRG12@users.noreply.github.com>
2022-10-26 14:40:29 -0500

    Tarjeta
    Agregamos una tarjeta para la estética de la vista, y tenga mejor vista.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,26 @@
  <h1 class="aling-text center" style="background-color: aliceblue"> Inicio actividad GitHub </h1>
 
 
-   <div class="container ">
+ <div class="card text-center mb-5 ml-5 fw-bold" style="width: 24rem; margin-left: 1rem">
+                
+        
+    <div class="card-header text-center">
+      
+    </div>
+    
 
-   </div>
-  
+    <div class="card-body">
+     
+    </div>
+
+
+
+    <div class="card-footer">
+        
+    </div>
+
+
+</div> 
 
      
     
